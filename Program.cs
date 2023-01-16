2e69a99b6f235b3afac16a3e6cb2e102eecb190c
steven-p-sutton <steven.p.sutton@gmail.com>
2023-01-16 19:17:59 +0000

    Try out Console in/out

--- Program.cs
+++ Program.cs
@@ -1,6 +1,40 @@
-﻿Console.WriteLine("Hello, World!");
+﻿
+internal class Program
+{
 
+    // References :
+    // https://learn.microsoft.com/en-us/dotnet/api/system.console?view=net-7.0
 
-// Try out Console Read/Write etc
+    // Try out Console Read/Write etc
+    // attach / start processes
+    // debug / non-debug
+    // standrad / non-standard input / output streams
+    private static void Main(string[] args)
+    {
+        try
+	    {
+            Console
+            Console.Write("Hello ");
+            Console.WriteLine("World!");
+
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Good day, ");
+            Console.Write(name);
+
+            Console.WriteLine("!");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+        finally 
+	    {
+            Console.Write("\n\nPress any to continue ...");
+            Console.ReadLine();
+        }
+    }
+}
 
 
