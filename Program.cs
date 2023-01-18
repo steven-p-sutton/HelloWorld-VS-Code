66ca5ac37adb0b1c60fbd2c5ce083aa07b6a89eb
steven-p-sutton <steven.p.sutton@gmail.com>
2023-01-18 11:05:11 +0000

    Partial Classes

--- Program.cs
+++ Program.cs
@@ -1,7 +1,6 @@
 ﻿
 internal class Program
 {
-
     // References :
     // https://learn.microsoft.com/en-us/dotnet/api/system.console?view=net-7.0
 
@@ -12,21 +11,27 @@ internal class Program
     private static void Main(string[] args)
     {
         try
-	    {
-            Console
-            Console.Write("Hello ");
-            Console.WriteLine("World!");
+        {
+            //Console.Write("Hello ");
+            //Console.WriteLine("World!");
 
-            Console.Write("Enter your name: ");
-            string name = Console.ReadLine();
+            //Console.Write("Enter your name: ");
+            //string name = Console.ReadLine();
 
-            Console.Write("Good day, ");
-            Console.Write(name);
+            //Console.Write("Good day, ");
+            //Console.Write(name);
 
-            Console.WriteLine("!");
+            //Console.WriteLine("!");
+
+            // https://www.w3schools.com/cs/cs_properties.php
+
+            Person myObj = new Person();
+            myObj.Name = "Steve";
+            myObj.Surname = "Sutton";
+            Console.WriteLine(myObj.Name + " " + myObj.Surname);
         }
         catch (Exception e)
-        {
+        { 
             Console.WriteLine(e.ToString());
         }
         finally 
@@ -35,6 +40,23 @@ internal class Program
             Console.ReadLine();
         }
     }
+    
+class Person
+{
+  private string name; // field
+private string sutton; // field
+
+  public string Name   // property
+  {
+    get { return name; }   // get method
+    set { name = value; }  // set method
+  }
+public string Surname   // property
+  {
+    get { return name; }   // get method
+    set { name = value; }  // set method
+  }
+}
 }
 
 
